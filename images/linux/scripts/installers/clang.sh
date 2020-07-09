7850404a9d3f3624e3a39c1d26ea9fd3aa697f2f
AshUK <github.AshUK@outlook.com>
2020-07-09 23:52:30 +0100

    make clang 10 default  on linux

--- images/linux/scripts/installers/clang.sh
+++ images/linux/scripts/installers/clang.sh
@@ -51,7 +51,7 @@ done
 
 rm llvm.sh
 
-# Make Clang 9 default
-update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-9 100
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-9 100
-update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-9 100
+# Make Clang 10 default
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-10 100
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 100
+update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-10 100
